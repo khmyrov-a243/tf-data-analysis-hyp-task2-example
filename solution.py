b0594dfec34c92d7aca5d3023c278d7901a182b8
khmyrov-a243 <128284168+khmyrov-a243@users.noreply.github.com>
2023-04-13 20:23:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ks_2samp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 973327975 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    # применяем тест Колмогорова-Смирнова к выборкам x и y
+    test_result = ks_2samp(x, y)
+
+    # сравниваем p-value с уровнем значимости
+    return test_result.pvalue < 0.04
